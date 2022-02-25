5a07461bfe7d0af7296583799e93114f9916b6b8
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2022-02-25 13:50:46 -0500

    finish date class

---
+++ ConceptsPOO1/ConceptsPOO1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO1;
+
+Console.WriteLine("Concepts POO");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1999, 11, 30));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
